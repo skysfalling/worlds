550b2b6d6a476d64ef9a17b1aa240f73d73353ae
Sky Casey <theocasey11@gmail.com>
2022-12-28 12:08:08 -0800

    init
    created link header and cards

---
+++ styles/linkheader.js
@@ -0,0 +1,29 @@
+const dropdownLink = document.querySelector('.dropdown-link');
+const links = document.querySelectorAll('.link-header a');
+let isDropdownOpen = false;
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 600) {
+    links.forEach(link => link.style.display = 'none');
+    isDropdownOpen = false;
+
+    dropdownLink.style.display = 'block';
+  } else {
+    links.forEach(link => link.style.display = 'block');
+    isDropdownOpen = true;
+
+  }
+});
+
+dropdownLink.addEventListener('click', () => {
+  if (isDropdownOpen) {
+    links.forEach(link => link.style.display = 'none');
+    isDropdownOpen = false;
+
+    dropdownLink.style.display = 'block';
+
+  } else {
+    links.forEach(link => link.style.display = 'block');
+    isDropdownOpen = true;
+  }
+});
